709a5b9bf682b98fef79638d5ceec2d8dc13de26
John Rey <john.rey.baylen.pro@gmail.com>
2022-12-04 03:49:33 -0500

    Client: - Remove .gitignore in src directory - Update page title - Remove Vite logo in public directory

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const ROOT_ELEMENT = document.getElementById('root')
+const { render } = ReactDOM.createRoot(ROOT_ELEMENT as HTMLElement)
+
+render(
+	<React.StrictMode>
+		<div>Hello World!</div>
+	</React.StrictMode>
 )

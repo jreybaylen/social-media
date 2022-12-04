66870a5ca9c2fdfa3b50bf0e6db2852d174a37fe
John Rey <john.rey.baylen.pro@gmail.com>
2022-12-04 04:26:08 -0500

    Client: - Add tailwind css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const ROOT_ELEMENT = document.getElementById('root')
-const { render } = ReactDOM.createRoot(ROOT_ELEMENT as HTMLElement)
+const REACT_DOM = ReactDOM.createRoot(ROOT_ELEMENT as HTMLElement)
 
-render(
+REACT_DOM.render(
 	<React.StrictMode>
-		<div>Hello World!</div>
+		<h1 className="text-3xl font-bold underline">
+			Hello world!
+		</h1>
 	</React.StrictMode>
 )
